170957b56784015f075c3b5953df40889e0d079b
Anton <anton.kharitonov@sap.com>
2024-01-03 12:14:00 +0100

    add variable

--- variables.tf
+++ variables.tf
@@ -2,3 +2,8 @@
 #  description = "Type of EC2 instance to provision"
 #  default     = "t3.nano"
 #}
+
+variable "cloud_run_service" {
+    name = "gcr-service-test"
+    location = "us-central1"
+}
